7537d26e8167c22d862d6189396f37ac86d9882b
Aiden <146897166+AidenFischer@users.noreply.github.com>
2024-12-12 17:20:19 -0700

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,1 +1,13 @@
+//Referencing MongoDB_Activities_Source_Code
 
+const db = require("../db");
+
+const userSchema = new db.Schema({
+    name: String,
+    email: String,
+    password: String
+});
+
+const User = db.model("User", userSchema);
+
+module.exports = User;
