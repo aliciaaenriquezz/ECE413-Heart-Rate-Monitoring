3cdb27db40298106ce7f62ea1110c731b94fc361
Aiden <146897166+AidenFischer@users.noreply.github.com>
2024-12-14 15:38:34 -0700

    Create login.js

---
+++ public/javascripts/login.js
@@ -0,0 +1,24 @@
+function login() {
+  let txdata = {
+      email: $('#email').val().toUpperCase(),
+      password: $('#password').val()
+  };
+  $.ajax({
+      url: '/users/logIn',
+      method: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify(txdata),
+      dataType: 'json'
+  })
+  .done(function (data, textStatus, jqXHR) {
+      localStorage.setItem("token", data.token);
+      window.location.replace("mainmenu.html");
+  })
+  .fail(function (jqXHR, textStatus, errorThrown) {
+      $('#rxData').html(JSON.stringify(jqXHR, null, 2));
+  });
+}
+
+$(function () {
+  $('#submit').click(login);
+});
