03aa3eb9fbfc9a97cca3a02312e507425175be2b
Alicia E <163488919+aliciaaenriquezz@users.noreply.github.com>
2024-12-16 21:45:10 -0700

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,14 +1,21 @@
-//Referencing MongoDB_Activities_Source_Code
+// Referencing MongoDB_Activities_Source_Code
 
+// Import the database connection from the '../db' file
 const db = require("../db");
 
+// Define the schema for the 'User' collection in MongoDB
 const userSchema = new db.Schema({
-    name: String,
-    email: String,
-    password: String,
-    lastAccess: { type: Date, default: Date.now}
+    name: String, // User's name as a string
+    email: String, // User's email as a string
+    password: String, // User's password as a string (Note: Passwords should be hashed for security)
+    lastAccess: { 
+        type: Date, // Date type to track user's last access time
+        default: Date.now // Defaults to the current date and time when a new user is created
+    }
 });
 
+// Create a model for the 'User' collection using the defined schema
 const User = db.model("User", userSchema);
 
+// Export the 'User' model so it can be used in other parts of the application
 module.exports = User;
