1c27c505cc32cce6e972b326665e13f728ebb58f
Alicia E <163488919+aliciaaenriquezz@users.noreply.github.com>
2024-12-16 22:09:21 -0700

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,21 @@
-// to use mongoDB
+// Importing mongoose to interact with MongoDB
 const mongoose = require("mongoose");
-mongoose.set('strictQuery', true);
-mongoose.connect("mongodb+srv://aenriquez45:HBq6xD4jFLRWGDVZ@heartratemonitor.8dia7.mongodb.net/", { useNewUrlParser: true, useUnifiedTopology:true });
 
+// Setting strict query mode to true, which ensures that queries will not include fields that are not defined in the schema
+mongoose.set('strictQuery', true);
+
+// Connecting to MongoDB using Mongoose. 
+// The connection string points to the MongoDB Atlas cluster. Replace this with your actual MongoDB URI.
+mongoose.connect("mongodb+srv://aenriquez45:HBq6xD4jFLRWGDVZ@heartratemonitor.8dia7.mongodb.net/", {
+    useNewUrlParser: true,    // Ensures the new URL parser is used to avoid deprecation warnings
+    useUnifiedTopology: true // Enables the new connection management engine for more stable and efficient connections
+})
+.then(() => {
+    console.log("Connected to MongoDB successfully.");
+})
+.catch((err) => {
+    console.error("Failed to connect to MongoDB:", err);
+});
+
+// Exporting mongoose instance so it can be used in other parts of the application
 module.exports = mongoose;
