0c1bfd4ecdb2106f7f619a6458ef2ba7fc9875a6
Alicia E <163488919+aliciaaenriquezz@users.noreply.github.com>
2024-12-16 21:55:42 -0700

    Update accountInfo.js

--- public/javascripts/accountInfo.js
+++ public/javascripts/accountInfo.js
@@ -1,18 +1,34 @@
 $(function (){
+    // When the document is ready, the following function is executed
+
     $.ajax({
-        url: '/users/status',
-        method: 'GET',
-        headers: { 'x-auth' : window.localStorage.getItem("token") },
-        dataType: 'json'
+        // Perform an AJAX request to the server
+        url: '/users/status', // The endpoint to fetch the user's status
+        method: 'GET', // The HTTP method is GET
+        headers: { 
+            'x-auth' : window.localStorage.getItem("token") // Attach the authentication token from local storage in the request header
+        },
+        dataType: 'json' // The expected response data type is JSON
     })
     .done(function (data, textStatus, jqXHR) {
-        //This way you can insert personalized data directly to the html web page
-        $('#name').html(data.map(obj => obj['name']));              //use element with id = "name"
-        $('#userEmail').html(data.map(obj => obj['email']));        //use element with id = "userEmail"
-        $('#fullName').val(data.map(obj => obj['name']).join());    //use input with id = "fullname"
-        $('#email').val(data.map(obj => obj['email']).join());      //use input with id = "email"
+        // If the request is successful, this function is executed
+        // Insert the data into the HTML page dynamically
+
+        $('#name').html(data.map(obj => obj['name'])); 
+        // Use element with id = "name" and set its HTML content to the user's name
+        
+        $('#userEmail').html(data.map(obj => obj['email'])); 
+        // Use element with id = "userEmail" and set its HTML content to the user's email
+        
+        $('#fullName').val(data.map(obj => obj['name']).join());  
+        // Use input with id = "fullName" and set its value to the user's name
+        
+        $('#email').val(data.map(obj => obj['email']).join()); 
+        // Use input with id = "email" and set its value to the user's email
     })
     .fail(function (jqXHR, textStatus, errorThrown) {
-        window.alert("Failure /javascripts/accountInfo.js");
+        // If the request fails, this function is executed
+        window.alert("Failure /javascripts/accountInfo.js"); 
+        // Show an alert box indicating failure
     });
 });
