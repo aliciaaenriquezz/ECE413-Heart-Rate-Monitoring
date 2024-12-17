9ff2f273bad1f36f5bd2f66761ffcf8243328228
Alicia E <163488919+aliciaaenriquezz@users.noreply.github.com>
2024-12-16 22:07:11 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
+// Importing the express module
 var express = require('express');
+
+// Creating a new router instance
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page route handler */
 router.get('/', function(req, res, next) {
+  // Rendering the 'index' view with a 'title' variable to be used in the template
   res.render('index', { title: 'Express' });
 });
 
+// Exporting the router so it can be used in other parts of the application
 module.exports = router;
